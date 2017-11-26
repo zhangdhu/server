c90f429b6b9d256da452cc96cc72e139905e2c18
张大虎 <zhangdahu@qiyi.com>
2017-11-26 21:30:09 +0800

    增加保存 获取所有接口

--- api/src/main/java/com/zdh/api/controller/UserInfoController.java
+++ api/src/main/java/com/zdh/api/controller/UserInfoController.java
@@ -5,6 +5,7 @@ import com.zdh.api.repository.UserInfoRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,14 +24,17 @@ public class UserInfoController {
     private UserInfoRepository userInfoRepository;
 
     @ApiOperation(value = "index", tags = "user", notes = "返回一个字符串")
-    @GetMapping()
-    public List<UserInfo> intdex() {
-        UserInfo userInfo = new UserInfo();
-        userInfo.setId(11L);
-        userInfo.setName("zhang");
-        userInfoRepository.save(userInfo);
+    @GetMapping("/list")
+    public List<UserInfo> get() {
         List<UserInfo> userInfoList = userInfoRepository.findAll();
-//        return userInfoRepository.save(userInfo);
         return userInfoList;
+    }
+
+    @ApiOperation(value = "index", tags = "user", notes = "返回一个字符串")
+    @PostMapping("/save")
+    public UserInfo save() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName("zhang");
+        return userInfoRepository.save(userInfo);
     }
 }
